bb93311b5a1733a1e30a848436de748df711953e
changsubkwak <paradise1732@gmail.com>
2022-11-21 19:52:04 +0900

    feat: (WIP) implement production code - refactoring solution - Tease Apart Inheritance

---
+++ src/main/java/solution/_9_tease_apart_inheritance/Deal.java
@@ -0,0 +1,6 @@
+package solution._9_tease_apart_inheritance;
+
+// Business Domain (ActiveDeal, PassiveDeal) View(TabularActiveDeal, TabularActiveDeal)
+// 클래스를 상속 구조가 아닌 위임 구조로 바꿔보기
+public class Deal {
+}
---
+++ src/main/java/solution/_9_tease_apart_inheritance/PresentationStyle.java
@@ -0,0 +1,4 @@
+package solution._9_tease_apart_inheritance;
+
+public class PresentationStyle {
+}
