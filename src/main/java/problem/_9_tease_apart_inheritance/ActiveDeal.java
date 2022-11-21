54248b217157051e69f9c1707a6b962b32b73ed5
changsubkwak <paradise1732@gmail.com>
2022-11-21 19:49:25 +0900

    feat: (WIP) implement production code - refactoring problem - Tease Apart Inheritance

---
+++ src/main/java/problem/_9_tease_apart_inheritance/ActiveDeal.java
@@ -0,0 +1,4 @@
+package problem._9_tease_apart_inheritance;
+
+public class ActiveDeal extends Deal {
+}
---
+++ src/main/java/problem/_9_tease_apart_inheritance/PassiveDeal.java
@@ -0,0 +1,4 @@
+package problem._9_tease_apart_inheritance;
+
+public class PassiveDeal extends Deal {
+}
