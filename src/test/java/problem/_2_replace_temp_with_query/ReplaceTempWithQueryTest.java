452a250b6bb5552beada94348cc0cb6563732f53
changsubkwak <paradise1732@gmail.com>
2022-11-26 11:18:51 +0900

    test: implement test case - Replace Temp with Query #1

---
+++ src/test/java/problem/_2_replace_temp_with_query/ReplaceTempWithQueryTest.java
@@ -0,0 +1,27 @@
+package problem._2_replace_temp_with_query;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static problem.CommonUtil.getOutputStream;
+
+class ReplaceTempWithQueryTest {
+
+    @DisplayName("quantity * price 값이 1000 초과할 때")
+    @Test
+    void test1() {
+        ReplaceTempWithQuery replaceTempWithQuery = new ReplaceTempWithQuery(1, 500);
+        assertThat(replaceTempWithQuery.getPrice()).isEqualTo(490);
+    }
+
+    @DisplayName("quantity * price 값이 1000 이하일 때")
+    @Test
+    void test2() {
+        ReplaceTempWithQuery replaceTempWithQuery = new ReplaceTempWithQuery(5, 500);
+        assertThat(replaceTempWithQuery.getPrice()).isEqualTo(2375);
+    }
+}
