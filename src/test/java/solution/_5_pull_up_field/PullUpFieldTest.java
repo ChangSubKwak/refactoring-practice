8bed6c277e561a156b82e2d7b5c7ac5ee813eb06
changsubkwak <paradise1732@gmail.com>
2022-11-29 22:19:57 +0900

    refactor: change @DisplayName comment

--- src/test/java/solution/_5_pull_up_field/PullUpFieldTest.java
+++ src/test/java/solution/_5_pull_up_field/PullUpFieldTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PullUpFieldTest {
-	@DisplayName("Pull Up Field를 수행이전의 객체의 getName메소드의 반환값과 Pull Up Field를 수행이후의 객체의 getName메소드의 반환값은 같다")
+	@DisplayName("Pull Up Field를 수행이전의 Engineer 객체의 getName메소드의 반환값과 Pull Up Field를 수행이후의 Engineer 객체의 getName메소드의 반환값은 같다")
 	@Test
 	void employee() {
 		problem._5_pull_up_field.Employee employee1 = new problem._5_pull_up_field.Engineer("changsub");
