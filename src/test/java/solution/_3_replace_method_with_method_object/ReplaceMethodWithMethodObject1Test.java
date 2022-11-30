0ebc6050d9a62ff03601dab328e93d8983561971
changsubkwak <paradise1732@gmail.com>
2022-11-30 23:16:36 +0900

    feat: add @DisplayName comment - Replace Method With Method Object

--- src/test/java/solution/_3_replace_method_with_method_object/ReplaceMethodWithMethodObject1Test.java
+++ src/test/java/solution/_3_replace_method_with_method_object/ReplaceMethodWithMethodObject1Test.java
@@ -1,5 +1,6 @@
 package solution._3_replace_method_with_method_object;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import solution._3_replace_method_with_method_object._1.ReplaceMethodWithMethodObject;
 
@@ -7,10 +8,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ReplaceMethodWithMethodObject1Test {
 
+	@DisplayName("delta값은 50으로 하고, gamma 메소드에 인자값을 100(입력값), 3(수량), 500(년수) 입력하면 349860을 반환한다")
 	@Test
-	void gamma() {
+	void gamma1() {
 		ReplaceMethodWithMethodObject replaceMethodWithMethodObject = new ReplaceMethodWithMethodObject(50);
 		assertThat(replaceMethodWithMethodObject.gamma(100, 3, 500)).isEqualTo(349860);
+	}
+
+	@DisplayName("delta값은 50으로 하고, gamma 메소드에 인자값을 100(입력값), 4(수량), 500(년수) 입력하면 349800을 반환한다")
+	@Test
+	void gamma2() {
+		ReplaceMethodWithMethodObject replaceMethodWithMethodObject = new ReplaceMethodWithMethodObject(50);
 		assertThat(replaceMethodWithMethodObject.gamma(100, 4, 500)).isEqualTo(349800);
 	}
 }
