e0507854c6554f48c279afa033118afc24d9f715
changsubkwak <paradise1732@gmail.com>
2022-12-09 17:51:55 +0900

    feat: change code - Replace Temp With Query

--- src/main/java/problem/_2_replace_temp_with_query/ReplaceTempWithQuery4.java
+++ src/main/java/problem/_2_replace_temp_with_query/ReplaceTempWithQuery4.java
@@ -12,10 +12,11 @@ public class ReplaceTempWithQuery4 {
     //TODO: 부수효과(side effect) 발생부분 분리하기
     // 참고 : https://en.wikipedia.org/wiki/Pure_function
     // 메소드설명 : 부수효과 발생 케이스를 만든 것
-    public double increaseQuantityAndDecreaseItemPriceAndGetPrice() {
+    public double getPrice() {
+        int basePrice = quantity * itemPrice;
         quantity++;             // 함수내 지역변수가 아닌 다른 변수 변경시 부수효과 발생 #1
         itemPrice -= 100;       // 두번째 부수효과 발생 #2
-        int basePrice = quantity * itemPrice;
+        basePrice = quantity * itemPrice;
 
         double discountFactor;
         if (basePrice > 1000) discountFactor = 0.95;
